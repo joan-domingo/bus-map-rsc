a1d0b3c2c01231187eaffa87e24d62ef2ff561fb
Joan Domingo <joands@gmail.com>
2026-03-10 21:39:59 +0100

    Revert "Add feedback button"
    This reverts commit 8bcf924c806214d9ed9b24e34637d9d5712d604a.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental: {
-    turbopackUseSystemTlsCerts: true,
-  },
   async headers() {
     return [
       {
